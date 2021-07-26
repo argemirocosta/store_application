d1e4e5021b5a41a75e138a8153f4e4148409522a
Argemiro <argemiro.pcosta@gmail.com>
2021-07-26 19:46:52 -0300

    Retirando report sem uso; Adicionando opção de relatório de vendas por dia;

--- src/main/br/com/storeapplication/controller/ReportJasperMB.java
+++ src/main/br/com/storeapplication/controller/ReportJasperMB.java
@@ -5,6 +5,7 @@ import br.com.storeapplication.util.RelatorioUtil;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -16,14 +17,15 @@ public class ReportJasperMB {
     public ReportJasperMB() {
     }
 
-    public void gerarRecibo(Integer idPagamento) throws IOException {
+    public void gerarReportVendaPorDia(Date dataInicio, Date dataFinal) throws IOException {
         String caminho = "/WEB-INF/relatorios/";
-        String relatorio = caminho + "recibo.jasper";
+        String relatorio = caminho + "vendas_por_dia.jasper";
         Map<String, Object> map = new HashMap<>();
 
-        map.put("id_pagamento", idPagamento);
+        map.put("datainicio", new java.sql.Date(dataInicio.getTime()));
+        map.put("datafim", new java.sql.Date(dataFinal.getTime()));
         map.put("REPORT_LOCALE", new Locale("pt", "BR"));
-        RelatorioUtil.executeReport(relatorio, map, "Recibo venda "+idPagamento+".pdf");
+        RelatorioUtil.executeReport(relatorio, map, "Vendas por dia.pdf");
     }
 
 }
