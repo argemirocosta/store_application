6649921f76a0d58fb4a978e3aebf677ddfd582a6
Argemiro github <argemirocosta@outlook.com>
2026-06-07 17:20:12 -0300

    Fixing database configurations to use env vars

--- src/main/br/com/storeapplication/factory/ConexaoBuilder.java
+++ src/main/br/com/storeapplication/factory/ConexaoBuilder.java
@@ -1,5 +1,6 @@
 package br.com.storeapplication.factory;
 
+import br.com.storeapplication.util.VerificadorUtil;
 
 public class ConexaoBuilder {
 
@@ -14,17 +15,27 @@ public class ConexaoBuilder {
             conexoes.setSenha("post");
         }
         else if(propriedades.Conexao.equals(Propriedades.Conexoes.DEPLOY)){
-            conexoes.setUrlBanco("jdbc:postgresql://10.100.33.197:5432/store_application");
-            conexoes.setUsuario("webadmin");
-            conexoes.setSenha("MKOpat83336");
+            conexoes.setUrlBanco(obterVariavelAmbiente("DB_URL_DEPLOY"));
+            conexoes.setUsuario(obterVariavelAmbiente("DB_USUARIO_DEPLOY"));
+            conexoes.setSenha(obterVariavelAmbiente("DB_SENHA_DEPLOY"));
         }
         else if(propriedades.Conexao.equals(Propriedades.Conexoes.PRODUCAO)){
-            conexoes.setUrlBanco("jdbc:postgresql://node73163-env-7907985.jelastic.saveincloud.net:11965/store_application");
-            conexoes.setUsuario("webadmin");
-            conexoes.setSenha("MKOpat83336");
+            conexoes.setUrlBanco(obterVariavelAmbiente("DB_URL_PRODUCAO"));
+            conexoes.setUsuario(obterVariavelAmbiente("DB_USUARIO_PRODUCAO"));
+            conexoes.setSenha(obterVariavelAmbiente("DB_SENHA_PRODUCAO"));
         }
 
         return conexoes;
+    }
+
+    private static String obterVariavelAmbiente(String nome) {
+        String valor = System.getenv(nome);
+
+        if(VerificadorUtil.verificarSeObjetoNuloOuVazio(valor)){
+            throw new IllegalStateException("Variável de ambiente " + nome + " não definida.");
+        }
+
+        return valor;
     }
 
 }
