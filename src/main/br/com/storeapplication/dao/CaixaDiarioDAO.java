aca53d7305a1a64e0a58e77f92aa0b6c214e8026
Argemiro github <argemirocosta@outlook.com>
2026-06-18 00:09:09 -0300

    Exibe saldo total do caixa no dialog após inserção sem fechá-lo
    Após adicionar um lançamento, o dialog permanece aberto e exibe o saldo
    atual do caixa (soma de todos os valores) em vermelho e negrito, dando
    ao usuário confirmação visual imediata do impacto do registro.

--- src/main/br/com/storeapplication/dao/CaixaDiarioDAO.java
+++ src/main/br/com/storeapplication/dao/CaixaDiarioDAO.java
@@ -9,8 +9,10 @@ import br.com.storeapplication.util.SessaoUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static br.com.storeapplication.shared.queries.CaixaDiarioDAOQueries.BUSCAR_VALOR_CAIXA;
 import static br.com.storeapplication.shared.queries.CaixaDiarioDAOQueries.INSERIR_CAIXA_DIARIO;
 
 public class CaixaDiarioDAO {
@@ -34,6 +36,27 @@ public class CaixaDiarioDAO {
 
         } catch (SQLException ex) {
             throw new ProjetoException(ex);
+        } finally {
+            try {
+                conexao.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public Double buscarDiferencaCaixa() {
+        conexao = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = conexao.prepareStatement(BUSCAR_VALOR_CAIXA);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getDouble("diferenca_caixa");
+            }
+            return 0.0;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
         } finally {
             try {
                 conexao.close();
--- src/main/br/com/storeapplication/shared/queries/CaixaDiarioDAOQueries.java
+++ src/main/br/com/storeapplication/shared/queries/CaixaDiarioDAOQueries.java
@@ -7,4 +7,7 @@ public class CaixaDiarioDAOQueries {
 
     public static final String INSERIR_CAIXA_DIARIO =
             "INSERT INTO vendas.caixa_diario (data, valor, usuario) VALUES (?,?,?)";
+
+    public static final String BUSCAR_VALOR_CAIXA =
+            "SELECT sum(valor) AS diferenca_caixa FROM vendas.caixa_diario";
 }
